ea191ef7b53c3aa5252b91ff2f656280a1b7ad5a
PedroBittencourtCosta <pedro.bittencourt.dev@gmail.com>
2025-12-20 21:32:32 -0300

    feat: ajustando os campos de usuario no adim

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,12 +3,23 @@ from django.contrib.auth.admin import UserAdmin
 from .models import Usuario
 
 class UsuarioAdmin(UserAdmin):
-    # Adiciona seus campos customizados nas telas de edição do Admin
+    list_display = ('username', 'email', 'sexo', 'cor_raca', 'data_nascimento', 'is_staff')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    list_filter = ('sexo', 'cor_raca', 'is_staff', 'is_superuser')
+    ordering = ('-date_joined',)
+
+    # Configuração para o formulário de EDIÇÃO
     fieldsets = UserAdmin.fieldsets + (
-        ('Informações Epidemiológicas', {'fields': ('sexo', 'cor_raca', 'estado_civil', 'data_nascimento')}),
+        ('Informações Epidemiológicas', {
+            'fields': ('sexo', 'cor_raca', 'estado_civil', 'data_nascimento')
+        }),
     )
+
+    # Configuração para o formulário de CRIAÇÃO (Novo Usuário)
     add_fieldsets = UserAdmin.add_fieldsets + (
-        ('Informações Epidemiológicas', {'fields': ('sexo', 'cor_raca', 'estado_civil', 'data_nascimento')}),
+        ('Informações Epidemiológicas', {
+            'fields': ('sexo', 'cor_raca', 'estado_civil', 'data_nascimento'),
+        }),
     )
 
 admin.site.register(Usuario, UsuarioAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -3,13 +3,30 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class Usuario(AbstractUser):
- 
+    # Opções para Cor/Raça [cite: 639]
+    COR_RACA_CHOICES = [
+        ('1', 'Branco'),
+        ('2', 'Preta'),
+        ('3', 'Parda'),
+        ('4', 'Amarela'),
+        ('5', 'Indígena'),
+    ]
+
+    # Opções para Estado Civil [cite: 639]
+    ESTADO_CIVIL_CHOICES = [
+        ('1', 'Solteiro(a)'),
+        ('2', 'Casado(a)'),
+        ('3', 'Com companheiro(a)'),
+        ('4', 'Viúvo(a)'),
+        ('5', 'Outro'),
+    ]
+
     SEXO_CHOICES = [('F', 'Feminino'), ('M', 'Masculino')]
-    
+
     sexo = models.CharField(max_length=1, choices=SEXO_CHOICES)
-    cor_raca = models.CharField(max_length=20) 
-    estado_civil = models.CharField(max_length=30) 
-    data_nascimento = models.DateField(null=True, blank=True) 
+    cor_raca = models.CharField(max_length=1, choices=COR_RACA_CHOICES) # [cite: 639]
+    estado_civil = models.CharField(max_length=1, choices=ESTADO_CIVIL_CHOICES) # [cite: 639]
+    data_nascimento = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return self.username
