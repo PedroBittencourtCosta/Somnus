9ee1bd591d08924d74b35abca5c5e993121d8b7b
PedroBittencourtCosta <pedro.bittencourt.dev@gmail.com>
2025-12-20 22:41:18 -0300

    feat: add o __str__ nas models

--- core/models.py
+++ core/models.py
@@ -7,6 +7,9 @@ class Questionario(models.Model):
     descricao = models.TextField(blank=True)
     data_criacao = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.titulo
+
 class Pergunta(models.Model):
     TIPO_CHOICES = [('MC', 'Múltipla Escolha'), ('TX', 'Texto Livre')]
     
@@ -15,10 +18,16 @@ class Pergunta(models.Model):
     ordem = models.IntegerField(default=0)
     tipo = models.CharField(max_length=2, choices=TIPO_CHOICES, default='MC')
 
+    def __str__(self):
+        return self.conteudo
+
 class Alternativa(models.Model):
     pergunta = models.ForeignKey(Pergunta, related_name='alternativas', on_delete=models.CASCADE)
     conteudo = models.CharField(max_length=255) 
     valor = models.IntegerField() 
+
+    def __str__(self):
+        return self.conteudo
 
 class RespostaQuestionario(models.Model):
     usuario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -43,3 +52,6 @@ class RegraEquacao(models.Model):
     class Meta:
         verbose_name = 'Regra de Cálculo'
         verbose_name_plural = 'Regras de Cálculos'
+
+    def __str__(self):
+        return self.nome
--- ethics/models.py
+++ ethics/models.py
@@ -7,6 +7,9 @@ class TCLE(models.Model):
     versao = models.FloatField(default=1.0)
     data_criacao = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.versao
+
 class AceiteTCLE(models.Model):
     usuario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     tcle = models.ForeignKey(TCLE, on_delete=models.CASCADE)
