0b34607694ce32ef749b5eba237831d9a6273253
PedroBittencourtCosta <pedro.bittencourt.dev@gmail.com>
2026-02-10 15:22:46 -0300

    fix: ajuste para arquivos static

--- somnus/settings.py
+++ somnus/settings.py
@@ -61,6 +61,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -221,3 +222,6 @@ JAZZMIN_UI_TWEAKS = {
 }
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Pasta onde o Railway vai coletar os arquivos (não precisa criar manualmente)
+STATIC_ROOT = BASE_DIR / "staticfiles"
