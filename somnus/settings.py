c56be882054d31dcf13905200a183aa512be58d4
PedroBittencourtCosta <pedro.bittencourt.dev@gmail.com>
2026-07-05 14:10:56 -0300

    fix: ajuste na leitura dos hosts

--- somnus/settings.py
+++ somnus/settings.py
@@ -35,7 +35,7 @@ DEBUG = env('DEBUG')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['127.0.0.1', 'localhost'])
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 AUTH_USER_MODEL = 'accounts.Usuario'
 
